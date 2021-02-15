a6b06f282d5a0d6ef199b2396467045134d2a1f3
sadsewd <[lacitiskarlis@gmail.com]>
2021-02-15 14:48:01 +0200

    Koda izmaiņas

--- src/GitHub.java
+++ src/GitHub.java
@@ -3,8 +3,9 @@ public class GitHub {
 
 	public static void main(String[] args) {
 	String vards = "uldis";
-	
+	int a = 10, b = 15, c = a+b;
 	System.out.println(vards);
+	System.out.println("a + b = "+c);
 	}
 
 }
